960cd6fff69964bda21176e060a2aa22a66f3832
hornsbym <32848675+hornsbym@users.noreply.github.com>
2018-04-09 02:07:37 -0400

    Added descriptions.py. Edited theThing.py, room...
    Added a descriptions file to hold all room descriptions and dialogue.
    
    To theThing.py...
    -Edited error messages
    -Added a a take option to turn function
    -Added give option to turn function
    -Added use option to turn function
    
    To person.py...
    -Added inventory to MacReady
    -Added methods to modify inventory
    
    Need to...
    -Finalize error message fixes
    -Blair object modification?
    -Allow players to find out whose blood is infected
    -Continue adding descriptions to game
    -Keep up with stats for game
    -Add endings to game

---
+++ descriptions.py
@@ -0,0 +1,15 @@
+"""
+Written by Mitchell Hornsby
+descriptions.py
+This file holds all dialogue and descriptions for the game The Thing by Mitchell Hornsby.
+"""
+roomDict = {"Blair's Room":"You are in a narrow room dug out of the ice. It is dimly lit; one lamp sits in the center of \
+the room, casting a weak blue light over everything. There is a door, chained shut, along the far wall. Approaching the \
+door, you see there is a small opening to look through. Inside, you find a short, pudgy man huddled in front of a computer \
+screen. Blair.","Dining Room":"You find yourself in a large, warm room. A small generator hums in the corner; beside \
+it sits an electric heater. A long table sits in the center of the room. Its chairs are all turned the wrong way and \
+lined up in rows. Clearly, there has recently been an important meeting here.","Kitchen":"The make-shift space barely \
+qualifies as a kitchen. It is dark, cold and dirty. Dirty plates are scattered piled on one of the wooden counters, and \
+the remains of last night's meal lay decaying on the carving table. The smell is almost too much to bear.",'Experiment \
+Room':"",'Hangar':"<Hangar description>","Foyer":"<Foyer description>","Dog Room":"<Dog Room description>","Thawing \
+Room":"<Thawing Room description>","Security Room":"<Security Room description>","Experiment Room":'<Experiment Room description>'}
